f6f43bf601e658984fc0b124b18daa36ea32770a
Philippe ENTZMANN <philippe.entzmann@gmail.com>
2023-12-07 11:50:15 +0000

    fix: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['../pp-widgets-demo'],
+  //extends: ['../pp-widgets-demo'],
+  extends: ['@paxpar-tech/pp-widgets-demo'],
   devtools: { enabled: true }
 })
